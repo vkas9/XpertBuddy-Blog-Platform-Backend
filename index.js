8b93dc4d716993bb02bdcb89ce0a92eb0870353d
Vikas <81951052+vkas9@users.noreply.github.com>
2024-11-16 14:45:03 +0530

    added cors domain

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://expert-client.onrender.com"],
+    origin: ["http://localhost:5173","https://xpertbuddy.vikas.engineer","https://expert-client.onrender.com"],
     credentials: true,
   })
 );
